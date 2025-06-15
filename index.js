2a77f29460914854abf74558710f7572e79bd41e
krishna-sharma23 <ks3851040@gmail.com>
2025-06-15 21:29:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ let temp = 0;
 let delay;
 let x;
 spans.forEach(span =>{
-    temp +=0.2;
+    temp +=0.15;
     delay = temp + "s";
-    span.style.animation = "showup 5s" ;
+    span.style.animation = "showup 3s" ;
     span.style.animationDelay = delay;
     span.style.animationFillMode = "forward";
     span.addEventListener('animationend', () => {
